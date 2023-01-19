271705a9615cb7a503c0d42d4d6bf6493aaccb4e
Daniel Sampar <daniel.sampar12@gmail.com>
2023-01-19 18:57:22 -0300

    first commit

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+
+import { hash } from 'bcrypt';
+
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('create')
+  async createUser(@Body() userData: Prisma.UserCreateInput): Promise<User> {
+    try {
+      const hashedPassword = await hash(userData.password, 10);
+
+      return await this.userService.create({
+        ...userData,
+        password: hashedPassword,
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new HttpException(
+            'An user with this email is already registerd.',
+            400,
+          );
+        }
+
+        throw new HttpException(error.message, 400);
+      }
+
+      if (error instanceof Prisma.PrismaClientValidationError) {
+        throw new HttpException(
+          'Request did not pass database client validation. What may have occurred: - An required field is missing. - Wrong type on a required field.',
+          400,
+        );
+      }
+
+      throw new HttpException(error.message, 500);
+    }
+  }
+}
---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return await this.prisma.user.create({ data });
+  }
+}
